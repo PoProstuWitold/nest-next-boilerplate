5676c0b65dc797652e576c2ec7bbdf0296e60be2
PoProstuWitold <witekzawada@o2.pl>
2021-09-13 19:50:16 +0200

    config

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,16 +1,35 @@
+import { config } from 'dotenv'
+config()
 import { NestFactory } from '@nestjs/core'
-import { ValidationPipe } from '@nestjs/common';
+import { BadRequestException, ValidationError, ValidationPipe } from '@nestjs/common';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { useContainer } from 'class-validator';
 import { AppModule } from './modules/main/app.module'
 import { setupSwagger } from './swagger'
+import * as express from 'express'
 
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule)
+    const app = await NestFactory.create<NestExpressApplication>(AppModule)
+
     setupSwagger(app)
-    app.enableCors()
-    app.useGlobalPipes(new ValidationPipe())
+
+    app.enableCors({
+        credentials: true,
+        origin: process.env.ORIGIN,
+        optionsSuccessStatus: 200
+    })
+    app.setGlobalPrefix('/api')
+    app.useGlobalPipes(new ValidationPipe({
+        stopAtFirstError: true,
+        whitelist: true,
+        forbidNonWhitelisted: true,
+        exceptionFactory: (errors: ValidationError[]) => {
+            new BadRequestException(errors)
+        }
+    }))
+    app.use(express.json())
     useContainer(app.select(AppModule), { fallbackOnErrors: true })
-    await app.listen(3000)
+    await app.listen(Number(process.env.PORT))
 }
 
 void bootstrap()
--- server/src/modules/main/app.module.ts
+++ server/src/modules/main/app.module.ts
@@ -1,9 +1,29 @@
 import { Module } from '@nestjs/common'
+import { TypeOrmModule, TypeOrmModuleAsyncOptions } from '@nestjs/typeorm'
+import { ConfigModule, ConfigService } from '@nestjs/config'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 
 @Module({
-    imports: [],
+    imports: [
+        TypeOrmModule.forRootAsync({
+            imports: [ConfigModule],
+            inject: [ConfigService],
+            useFactory: (configService: ConfigService) => {
+                return {
+                    type: configService.get('DB_TYPE'),
+                    host: configService.get('DB_HOST'),
+                    port: configService.get('DB_PORT'),
+                    username: configService.get('DB_USERNAME'),
+                    password: configService.get('DB_PASSWORD'),
+                    database: configService.get('DB_DATABASE'),
+                    entities: [__dirname + './../**/**.entity{.ts,.js}'],
+                    synchronize: configService.get('DB_SYNC') === 'true'
+                } as TypeOrmModuleAsyncOptions
+            }
+        }),
+        ConfigModule
+    ],
     controllers: [AppController],
     providers: [AppService]
 })
