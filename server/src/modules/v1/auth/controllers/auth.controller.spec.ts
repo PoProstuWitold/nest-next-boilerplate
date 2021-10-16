b4466910fb6c797807b88587265dcbc94e955789
PoProstuWitold <witekzawada@o2.pl>
2021-10-16 14:50:59 +0200

    controller tests setup

--- server/src/modules/v1/auth/controllers/auth.controller.spec.ts
+++ server/src/modules/v1/auth/controllers/auth.controller.spec.ts
@@ -1,35 +1,56 @@
 import { Test, TestingModule } from '@nestjs/testing'
 import { AuthController } from './auth.controller'
 import { AuthService } from '../services/auth.service'
+import { ConfigModule } from '@nestjs/config'
+import { UserModule } from '../../../../modules/v1/user/user.module'
+import { getRepositoryToken, TypeOrmModule } from '@nestjs/typeorm'
+import { User } from '../../../../common/entities'
+import { JwtModule } from '@nestjs/jwt'
+import { createJwtConfiguration, createTestConfiguration } from '../../../../../test/test-utils'
+import { UserRepository } from '../../../../modules/v1/user/repositories/user.repository'
 
 describe('AuthController', () => {
     let module: TestingModule
     let controller: AuthController
     let service: AuthService
-    
-    beforeEach(async () => {
+    let repository: UserRepository
+
+    beforeAll(async () => {
         module = await Test.createTestingModule({
+            imports: [
+                ConfigModule.forRoot({
+                    isGlobal: true
+                }),
+                UserModule,
+                TypeOrmModule.forRootAsync(createTestConfiguration([User])),
+                TypeOrmModule.forFeature([User]),
+                JwtModule.registerAsync(createJwtConfiguration())
+            ],
             controllers: [AuthController],
             providers: [
-                {
-                    provide: AuthService,
-                    useValue: {
-                        register: jest.fn().mockResolvedValue({}),
-                        login: jest.fn().mockResolvedValue({}),
-                        logout: jest.fn().mockResolvedValue({}),
-                        socialProviderLogin: jest.fn().mockResolvedValue({}),
-                    }
-                }
+                AuthService
             ]
         }).compile()
         controller = module.get<AuthController>(AuthController)
+        service = module.get<AuthService>(AuthService)
+        repository = module.get<UserRepository>(getRepositoryToken(User))
+        repository.clear()
     })
 
     afterAll(async () => {
+        repository.clear()
         module.close()
     })
 
     it('Should be defined', () => {
+        expect(module).toBeDefined()
+    })
+
+    it('Should be defined', () => {
         expect(controller).toBeDefined()
+    })
+
+    it('Should be defined', () => {
+        expect(service).toBeDefined()
     })
 })
--- server/src/modules/v1/user/controllers/user.controller.spec.ts
+++ server/src/modules/v1/user/controllers/user.controller.spec.ts
@@ -1,23 +1,55 @@
+import { ConfigModule } from '@nestjs/config'
 import { Test, TestingModule } from '@nestjs/testing'
+import { getRepositoryToken, TypeOrmModule } from '@nestjs/typeorm'
+import { User } from '../../../../common/entities'
+import { createTestConfiguration } from '../../../../../test/test-utils'
+import { UserRepository } from '../repositories/user.repository'
+import { UserService } from '../services/user.service'
 import { UserController } from './user.controller'
 
 describe('UserController', () => {
     let module: TestingModule
+    let service: UserService
+    let repository: UserRepository
     let controller: UserController
 
-    beforeEach(async () => {
+    beforeAll(async () => {
         module = await Test.createTestingModule({
-            controllers: [UserController]
+            imports: [
+                ConfigModule.forRoot({
+                    isGlobal: true
+                }),
+                TypeOrmModule.forRootAsync(createTestConfiguration([User])),
+                TypeOrmModule.forFeature([User])
+            ],
+            controllers: [
+                UserController
+            ],
+            providers: [
+                UserService
+            ]
         }).compile()
 
         controller = module.get<UserController>(UserController)
+        service = module.get<UserService>(UserService)
+        repository = module.get<UserRepository>(getRepositoryToken(User))
+        repository.clear()
     })
 
     afterAll(async () => {
+        repository.clear()
         module.close()
     })
 
-    it('should be defined', () => {
+    it('Should be defined', () => {
+        expect(module).toBeDefined()
+    })
+
+    it('Should be defined', () => {
         expect(controller).toBeDefined()
+    })
+
+    it('Should be defined', () => {
+        expect(service).toBeDefined()
     })
 })
