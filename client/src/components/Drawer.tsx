be72066b933dcc669054b0a622e93620db8f8859
PoProstuWitold <witekzawada@o2.pl>
2022-05-12 02:09:45 +0200

    positioning

--- client/src/components/Drawer.tsx
+++ client/src/components/Drawer.tsx
@@ -9,16 +9,15 @@ const Drawer: React.FC<DrawerProps> = ({ children, className, type }) => {
 
     return (
         <div
-        style={{ minHeight: 'calc(100vh + 5rem)' }}
-        className={`fixed drawer ${type === "mobile" ? "drawer-mobile" : ""}`}>
+        className={`relative drawer ${type === "mobile" ? "drawer-mobile" : ""}`}>
             <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
-            <div className="flex flex-col items-center p-5 drawer-content bg-base-100">
+            <div className={`flex flex-col items-center p-5 ${type === "mobile" ? "lg:ml-80" : ""} drawer-content bg-base-100`}>
                 {/* <!-- Page content here --> */}
                 {children}
             </div> 
-            <div className="drawer-side bg-base-100">
-                <label htmlFor="my-drawer-2" className="bg-base-200 drawer-overlay"></label> 
-                <ul className="p-4 overflow-y-auto menu w-80 bg-base-200 text-base-content">
+            <div className="fixed drawer-side bg-base-100"  style={{ height: '2000px' }}>
+                <label htmlFor="my-drawer-2" className="bg-base-200 drawer-overlay"  style={{ height: '2000px' }}></label> 
+                <ul className="fixed p-4 overflow-y-auto menu w-80 bg-base-200 text-base-content" style={{ height: '2000px' }}>
                 {/* <!-- Sidebar content here --> */}
                 <li><a>Sidebar Item 1</a></li>
                 <li><a>Sidebar Item 2</a></li>
--- client/src/components/NavBar.tsx
+++ client/src/components/NavBar.tsx
@@ -71,14 +71,14 @@ export const NavBar: React.FC<NavBarProps> = () => {
                     <li tabIndex={0}>
                         <a>
                         <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 21a4 4 0 01-4-4V5a2 2 0 012-2h4a2 2 0 012 2v12a4 4 0 01-4 4zm0 0h12a2 2 0 002-2v-4a2 2 0 00-2-2h-2.343M11 7.343l1.657-1.657a2 2 0 012.828 0l2.829 2.829a2 2 0 010 2.828l-8.486 8.485M7 17h.01"></path></svg>
+                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M7 21a4 4 0 01-4-4V5a2 2 0 012-2h4a2 2 0 012 2v12a4 4 0 01-4 4zm0 0h12a2 2 0 002-2v-4a2 2 0 00-2-2h-2.343M11 7.343l1.657-1.657a2 2 0 012.828 0l2.829 2.829a2 2 0 010 2.828l-8.486 8.485M7 17h.01"></path></svg>
                             <p>Theme</p>
                         <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z"/></svg>
                         </a>
                         <ul className="z-50 w-full p-2 border border-t-0 bg-base-100">
                             {
-                                Themes.map((theme) => (
-                                    <li onClick={() => setTheme(theme.name.toLowerCase())}><a>{theme.name}</a></li>
+                                Themes.map((theme, index) => (
+                                    <li key={index} onClick={() => setTheme(theme.name.toLowerCase())}><a>{theme.name}</a></li>
                                 ))
                             }
                         </ul>
