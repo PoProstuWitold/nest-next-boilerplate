b573cecca69e45fe04d86b2224750801cdc3b9ec
PoProstuWitold <witekzawada@o2.pl>
2023-01-08 14:33:09 +0100

    api docs fix

--- server/src/common/swagger/index.ts
+++ server/src/common/swagger/index.ts
@@ -1,4 +1,4 @@
-import { INestApplication } from '@nestjs/common'
+import { INestApplication, Logger } from '@nestjs/common'
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger'
 import {
   SWAGGER_API_ROOT,
@@ -8,13 +8,20 @@ import {
 } from './constants'
 
 export const setupSwagger = (app: INestApplication) => {
+	const server = 'http://localhost:4000/api'
+	const docs = 'http://localhost:4000/api/docs'
+
     const options = new DocumentBuilder()
         .setTitle(SWAGGER_API_NAME)
         .setDescription(SWAGGER_API_DESCRIPTION)
         .setVersion(SWAGGER_API_CURRENT_VERSION)
         .addCookieAuth()
-        .addServer('http://localhost:4000/api/v1')
+        .addServer(server)
         .build()
+
     const document = SwaggerModule.createDocument(app, options)
     SwaggerModule.setup(SWAGGER_API_ROOT, app, document)
+
+    const logger = new Logger('Documentation')
+	logger.log(`API Documentation for "${server}" is avaible at "${docs}"`)
 }
